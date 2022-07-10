2e7ee4be8a49bb9346941f7853725ae562ad9160
dakone22 <35692622+dakone22@users.noreply.github.com>
2022-07-10 20:39:08 +0300

    fixed

--- media/lua/server/NoDisassemblingPlease/NoPickingUpPlease.lua
+++ media/lua/server/NoDisassemblingPlease/NoPickingUpPlease.lua
@@ -22,55 +22,46 @@ end
 
 NoPickingUpPlease.ISMoveableCursor_isValid = ISMoveableCursor.isValid
 function ISMoveableCursor:isValid(square)
-    if isClient() then
-        local trust = 0
-        local player = getPlayer()
+    local player = getPlayer()
 
-        if player:getModData()["voc:trust"] then
-            trust = player:getModData()["voc:trust"]
-        else
+    local safehouse = SafeHouse.getSafeHouse(square)
+    local safehouseArea = false
+    local safehouseMember = false
+
+    if safehouse then
+        local access = NoDisassemblingPlease.getSafehouseAccessLevel(player, safehouse)
+
+        safehouseArea = true
+        safehouseMember = access ~= "None"
+    end
+
+    if SandboxVars.NoDisassemblingPlease.NoDisassembling then
+        if ISMoveableCursor.mode[self.player] == "scrap" then
+            if safehouseArea and safehouseMember then
+                return NoPickingUpPlease.ISMoveableCursor_isValid(self, square)
+            end
             return false
         end
+    end
 
-        local safehouse = SafeHouse.getSafeHouse(square)
-        local safehouseArea = false
-        local safehouseMember = false
+    if SandboxVars.NoDisassemblingPlease.NoPickingUp then
 
-        if safehouse then
-            local access = NoDisassemblingPlease.getSafehouseAccessLevel(player, safehouse)
-
-            safehouseArea = true
-            safehouseMember = access ~= "None"
-        end
-
-        if SandboxVars.NoDisassemblingPlease.NoDisassembling then
-            if ISMoveableCursor.mode[self.player] == "scrap" then
-                if safehouseArea and safehouseMember then
-                    return NoPickingUpPlease.ISMoveableCursor_isValid(self, square)
-                end
-                return false
-            end
-        end
-
-        if SandboxVars.NoDisassemblingPlease.NoPickingUp then
-
-            local objects = self.objectListCache or self:getObjectList()
-            local object = objects[self.objectIndex]
-            if object and object.object and object.object.getSprite then
-                object = object.object
-                local props = object:getSprite():getProperties()
-                if props:Is(IsoFlagType.container) then
-                    local containerType = object:getContainer():getType()
-                    if NoPickingUpPlease.whitelist[containerType] or  -- allow whitelist
-							props:Is(IsoFlagType.attachedSurface) or  -- allow ???
-							(safehouseArea and safehouseMember) then  -- allow in safehouse
-                        return NoPickingUpPlease.ISMoveableCursor_isValid(self, square)
-                    end
+        local objects = self.objectListCache or self:getObjectList()
+        local object = objects[self.objectIndex]
+        if object and object.object and object.object.getSprite then
+            object = object.object
+            local props = object:getSprite():getProperties()
+            if props:Is(IsoFlagType.container) then
+                local containerType = object:getContainer():getType()
+                if not (NoPickingUpPlease.whitelist[containerType] or  -- allow whitelist
+                        props:Is(IsoFlagType.attachedSurface) or       -- allow ???
+                        (safehouseArea and safehouseMember)) then      -- allow in safehouse
+                    NoPickingUpPlease.ISMoveableCursor_isValid(self, square)
                     return false
                 end
             end
         end
-
-        return NoPickingUpPlease.ISMoveableCursor_isValid(self, square)
     end
+
+    return NoPickingUpPlease.ISMoveableCursor_isValid(self, square)
 end
